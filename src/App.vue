898ea23ac89681388718850f7669572cd4fe5c6d
yongsoo.choi <yongsoo.choi@hnine.com>
2021-01-19 17:16:14 +0900

    Revert "메뉴 링크 에러 디버깅을 위한 속성 변경"
    This reverts commit 3397098639ab54c751e06ad8b425e71a5ce2430c.

--- src/App.vue
+++ src/App.vue
@@ -67,7 +67,7 @@
         <v-list-item
           v-for="menu in menus"
           v-bind:key="menu.id"
-          v-bind:href="menu.path"
+          v-bind:to="menu.path"
         >
           <v-list-item-icon>
             <v-icon>mdi-folder</v-icon>
