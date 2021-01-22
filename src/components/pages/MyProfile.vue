6ce55d090c116ceddb928a2bd748fb19beba36c8
yongsoo.choi <yongsoo.choi@hnine.com>
2021-01-22 15:33:01 +0900

    프로필 페이지에 Parallax 컴포넌트 적용

--- src/components/pages/MyProfile.vue
+++ src/components/pages/MyProfile.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>
+  <v-parallax
+    dark
+    height="600"
+    src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+  >
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-thin mb-4">
+          Parallax Profile
+        </h1>
+        <h4 class="subheading">
+          Build your application today!
+        </h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+  <!-- <div>
     <h1>My Profile</h1>
     <div v-show="false">보임?</div>
-  </div>
+  </div> -->
 </template>
 
 <script>
